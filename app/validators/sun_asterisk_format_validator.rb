8dc158ccfadfe3d8bc5b89fc8917fb9f9a027a18
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2021-06-22 10:27:37 +0700

    Add params to i18n

--- app/validators/sun_asterisk_format_validator.rb
+++ app/validators/sun_asterisk_format_validator.rb
@@ -2,6 +2,6 @@ class SunAsteriskFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value && value.match?(/\A([^@\s]+)@(sun-asterisk.com)\z/)
 
-    record.errors.add attribute, :invalid_sun_asterisk_format
+    record.errors.add attribute, :invalid_sun_asterisk_format, value: value
   end
 end
