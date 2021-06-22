f5f5f72c13d696e822f766f9ed2a456d04c575ff
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2021-06-22 09:18:53 +0700

    validate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   validates :email, presence: true
+
+  validate :email_sun_asterisk_format
+
+  private
+
+  def email_sun_asterisk_format
+    return if email.to_s.match?(/\A([^@\s]+)@(sun-asterisk.com)\z/)
+
+    errors.add :email, :invalid
+  end
 end
